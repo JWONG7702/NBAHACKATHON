a731b07ca31255ab4347cf2f26b0a891aa92a450
k-gong <31123173+k-gong@users.noreply.github.com>
2017-08-18 22:04:42 -0400

    Add files via upload

--- read_nba_excel.py
+++ read_nba_excel.py
@@ -8,8 +8,8 @@ filename="Analytics_Attachment.xlsx"
 divisions_table=pd.read_excel(filename, sheetname=0, header=0)
 games=pd.read_excel(filename, sheetname=1, header=0)
 
-East_div= divisions_table[divisions_table.Conference_id=="East"]
-West_div= divisions_table[divisions_table.Conference_id=="West"] 
+East_div= divisions_table.loc[divisions_table.Conference_id=="East", :]
+West_div= divisions_table.loc[divisions_table.Conference_id=="West", :] 
 
 #Generates new columns
 East_div.loc[:,"Wins"]=0 
@@ -18,6 +18,7 @@ East_div.loc[:,"Games Left"]=82
 East_div.loc[:, "Eliminated"]=0
 East_div.loc[:, "Playoffs"]=0
 East_div.loc[:, "Date"]=""
+East_div.loc[:, "Total Points"]=0
 
 West_div.loc[:,"Wins"]=0 
 West_div.loc[:,"Losses"]=0
@@ -25,6 +26,7 @@ West_div.loc[:,"Games Left"]=82
 West_div.loc[:,"Eliminated"]=0
 West_div.loc[:, "Playoffs"]=0
 West_div.loc[:, "Date"]=""
+West_div.loc[:, "Total Points"]=0
 
 def add_win(team):
 	#function to add a win to winning team
@@ -51,6 +53,16 @@ def add_loss(team):
 		West_div.loc[West_div['Team_Name']==team, "Games Left"]-=1
 	else:
 		print("Uh Oh...")
+def add_points(team, points):
+	#Adds points to get total points in a season
+	conf=divisions_table[divisions_table.Team_Name==team]
+	conf=conf.iloc[0]["Conference_id"]
+	if conf=="East":
+		East_div.loc[East_div['Team_Name']==team, "Total Points"]+=points
+	elif conf=="West":
+		West_div.loc[West_div['Team_Name']==team, "Total Points"]+=points
+	else:
+		print("Uh Oh...")
 
 def check_elim(division, date):
 	#Function to check if any teams have been eliminated because of games
@@ -72,19 +84,22 @@ def check_play(division, date):
 			division.loc[index]=row
 	return division
 
-
 for index, row in games.iterrows():
-	date=row["Date"].split(" ")[0]
+	date=row["Date"]
 	if row["Winner"]=="Home":
 		w=row["Home Team"]
 		l=row["Away Team"]
 		add_win(w)
 		add_loss(l)
+		add_points(w, row["Home Score"])
+		add_points(l, row["Away Score"])
 	elif row["Winner"]=="Away":
 		w=row["Away Team"]
 		l=row["Home Team"]
 		add_win(w)
 		add_loss(l)
+		add_points(l, row["Home Score"])
+		add_points(w, row["Away Score"])
 	else:
 		print("Uh Oh...")
 
@@ -97,8 +112,8 @@ for index, row in games.iterrows():
 	East_div=check_play(East_div, date)
 	West_div=check_play(West_div, date)
 		
-print(East_div)
-print(West_div)
+East_div.to_excel("Playoff_Results.xlsx", sheet_name="East")
+West_div.to_excel("Playoff_Results.xlsx", sheet_name="West")
 
 
 
